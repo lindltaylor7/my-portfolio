8beb15fa05a008d68f3ff06386436d7d1ea250af
digitechgrouperu <digitechgrouperu@gmail.com>
2025-11-05 21:37:29 -0500

    udpating meta website

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,6 +11,41 @@ export default defineNuxtConfig({
         { rel: "icon", type: "image/png", href: "/jairpl.ico" },
         { rel: "apple-touch-icon", href: "/jairpl.ico" },
       ],
+      meta: [
+        {
+          name: "description",
+          content:
+            "Portafolio personal de Jair Pariona, desarrollador FullStack especializado en experiencias web inmersivas y diseño tecnológico.",
+        },
+        { name: "author", content: "Jair Pariona" },
+        { name: "robots", content: "index, follow" },
+
+        // Open Graph (para Facebook, LinkedIn)
+        { property: "og:title", content: "Jair Pariona | FullStack Dev" },
+        {
+          property: "og:description",
+          content:
+            "Explora mis proyectos, blog y experiencia en desarrollo web y móvil.",
+        },
+        {
+          property: "og:image",
+          content: "https://jairpl.com/lind/og-image.jpg",
+        },
+        { property: "og:url", content: "https://jairpl.com/lind" },
+        { property: "og:type", content: "website" },
+
+        // Twitter Card
+        { name: "twitter:card", content: "summary_large_image" },
+        { name: "twitter:title", content: "Jair Pariona | FullStack Dev" },
+        {
+          name: "twitter:description",
+          content: "Desarrollador FullStack apasionado por el diseño digital.",
+        },
+        {
+          name: "twitter:image",
+          content: "https://jairpl.com/lind/og-image.jpg",
+        },
+      ],
     },
     baseURL: "/",
     buildAssetsDir: "/_nuxt/",
