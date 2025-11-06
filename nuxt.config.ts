641ad2f64567d416bdd4189ddcfae5bc91c6211f
digitechgrouperu <digitechgrouperu@gmail.com>
2025-11-06 10:25:43 -0500

    update fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,7 @@ export default defineNuxtConfig({
     preset: "static",
     prerender: {
       crawlLinks: true,
-      routes: ["/", "/blog/test"],
+      routes: ["/"],
     },
   },
   app: {
