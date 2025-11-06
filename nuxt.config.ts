436d1ba13ab870bb69d770d8cc9dff2283e30068
digitechgrouperu <digitechgrouperu@gmail.com>
2025-11-05 19:41:27 -0500

    fix: use static Nuxt build for Hostinger

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: true,
+  nitro: {
+    preset: "static",
+  },
   app: {
     baseURL: "/lind/",
     buildAssetsDir: "/_nuxt/",
